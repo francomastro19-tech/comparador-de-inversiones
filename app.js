ccded3d437e659f1997544babc11985854286353
francomastro19-tech <francomastro19@gmail.com>
2025-09-20 13:53:52 -0300

    Update app.js
    Signed-off-by: francomastro19-tech <francomastro19@gmail.com>

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const container = document.getElementById('spreadsheet');
 const hot = new Handsontable(container, {
   data: [
-      ["Nombre de la Inversión (Ticker)", "Cantidad Comprada", "Precio de Compra (USD)", "Precio Actual (USD)", "Valor Invertido (USD)", "Valor Actual (USD)", "Ganancia/Perdida (%)"],,
+         ["Nombre de la Inversión (Ticker)", "Cantidad Comprada", "Fecha de Compra (YYYY-MM-DD)", "Precio de Compra (USD)", "Precio Actual (USD)", "Valor Invertido (USD)", "Valor Actual (USD)", "Ganancia/Perdida (%)"],,,
     ["CEDEAR.MELI", 5, 15000, "", "", "", ""],
     ["AAPL", 10, 150, "", "", "", ""],
     ["GD30", 2, 50000, "", "", "", ""]
